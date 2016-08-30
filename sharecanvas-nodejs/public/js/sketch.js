b99f73b79575a549f53cf27e16e8b785c8e56cbd
James Kong <k.kong@actimirror.com>
2016-08-30 13:43:19 +0800

    update front end

--- sharecanvas-nodejs/public/js/sketch.js
+++ sharecanvas-nodejs/public/js/sketch.js
@@ -2,9 +2,25 @@ var socket = io();
 var sketches = {};
 var localColor 	 = {};
 var id 		 = -1;
+
+var prev = new Date();
+var startPos = {x:50,y:50};
+var endPos = {x:590,y:430};
+var margin = 100;
+var rectsize = 10;
+var maxLength = 320*144;
 function setup(){
 	createCanvas(640, 480);
+	
+	startPos = {x:margin,y:margin};
+	endPos = {x:width-(margin),y:height-(margin)};
+	
+	rectsize = width/(width-(margin*2));
 
+	background(125);
+	fill(0,0,0);
+	rect(startPos.x,startPos.y, endPos.x- startPos.x, endPos.y - startPos.y);
+	console.log("rectsize " + rectsize);
 	socket.on('setup',function (message){
 		// console.log(message);
 
@@ -26,7 +42,7 @@ function setup(){
 			sketches[_id] = {color:c, points:[]};
 		}
 		sketches[_id].points.push( object.point );
-		if ( sketches[_id].points.length > 500 ){
+		if ( sketches[_id].points.length > maxLength ){
 			sketches[_id].points.shift();
 		}
 		// renderCanvas();
@@ -39,11 +55,14 @@ function setup(){
 		if ( sketches[_id] ){
 			delete sketches[_id];
 		}
-		background(255);
+		// background(0);
 		newDrawing();
 	})
 }
 function newDrawing(){
+	background(125);
+	fill(0,0,0);
+	rect(startPos.x,startPos.y, endPos.x- startPos.x, endPos.y - startPos.y);
 	for ( var _id in sketches ){
 		//console.log(sketches[_id]);
 		var c = sketches[_id].color;
@@ -52,29 +71,37 @@ function newDrawing(){
 		if ( pts.length > 0 ){			
 			
 			for ( var i=1, len = pts.length; i<len; i++){
+				noStroke();
 				fill(c.r , c.g , c.b);
-				ellipse(pts[i].x, pts[i].y, 10 ,10);
+				rect(pts[i].x, pts[i].y, rectsize ,rectsize);
 			}
 			
 		}
 	}
 }
 function mouseDragged(){
-	var point = {point:{x:mouseX,y:mouseY},id:id,color:localColor};
+	var curr = new Date();
+	var diff = get_time_diff_mis(prev);
+	prev = curr;
+	var mX = parseInt(mouseX);
+	var mY = parseInt(mouseY);
+	if(mX > startPos.x && mX < endPos.x && mY > startPos.y && mY < endPos.y){
+		var point = {point:{x:mX,y:mY},id:id,color:localColor,diff:diff};
 
-	if ( socket.connected ){
-		sketches[id].points.push( point.point );
-		if ( sketches[id].points.length > 500 ){
-			sketches[id].points.shift();
-		}
-		// var msg = JSON.stringify(point);
+		if ( socket.connected ){
+			sketches[id].points.push( point.point );
+			if ( sketches[id].points.length > 500 ){
+				sketches[id].points.shift();
+			}
+			// var msg = JSON.stringify(point);
 		
-		socket.emit('point',point);
-		// renderCanvas();
+			socket.emit('point',point);
+			// renderCanvas();
+		}
 	}
 }
 function draw(){
-
+	
 	// newDrawing();
 	// if (mouseIsPressed) {
 	// 	fill(0);
@@ -84,4 +111,31 @@ function draw(){
 	// ellipse(mouseX, mouseY, 80, 80);
 
 	
+}
+
+function get_time_diff_mis( datetime )
+{
+	var datetime = typeof datetime !== 'undefined' ? datetime : "2014-01-01 01:02:03.123456";
+
+	var datetime = new Date( datetime ).getTime();
+	var now = new Date().getTime();
+
+	if( isNaN(datetime) )
+	{
+		return "";
+	}
+
+    // console.log( datetime + " " + now);
+
+    if (datetime < now) {
+    	var milisec_diff = now - datetime;
+    }else{
+    	var milisec_diff = datetime - now;
+    }
+    return milisec_diff
+    // var days = Math.floor(milisec_diff / 1000 / 60 / (60 * 24));
+
+    // var date_diff = new Date( milisec_diff );
+
+    // return days + " Days "+ date_diff.getHours() + " Hours " + date_diff.getMinutes() + " Minutes " + date_diff.getSeconds() + " Seconds";
 }
